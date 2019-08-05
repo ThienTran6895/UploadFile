58c36dc18cbfbff88baafe88d9af8245b44ecdbf
Tran Cong Thien <thientc@matbao.com>
2019-08-05 17:22:17 +0700

    Version 1.02

--- UploadFile/UploadFile/Controllers/TablesController.cs
+++ UploadFile/UploadFile/Controllers/TablesController.cs
@@ -26,21 +26,36 @@ namespace UploadFile.Controllers
 
         // GET: Tables/Upload
         [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase files)
+        public JsonResult Upload()
         {
-            File model = new File();
-            List<File> list = new List<File>();
-            //DataTable 
-            if (files != null)
+            for (int i = 0; i < Request.Files.Count; i++)
             {
-                var Extension = Path.GetExtension(files.FileName);
-                var fileName = "my-file-" + DateTime.Now.ToString("ddMMyyyyHHmmssfff") + Extension;
-                string path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
-                model.FileUrl = Url.Content(Path.Combine("~/UploadedFiles/", fileName));
-                model.FileName = fileName;
+                HttpPostedFileBase file = Request.Files[i]; //Uploaded file
+                                                            //Use the following properties to get file's name, size and MIMEType
+                int fileSize = file.ContentLength;
+                string fileName = file.FileName;
+                string mimeType = file.ContentType;
+                System.IO.Stream fileContent = file.InputStream;
+                //To save file, use SaveAs method
+                file.SaveAs(Server.MapPath("~/UploadFiles/") + fileName); //File will be saved in application root
             }
-                return View();
+            return Json("Uploaded " + Request.Files.Count + " files");
         }
+        //public ActionResult Upload(HttpPostedFileBase files)
+        //{
+        //    File model = new File();
+        //    List<File> list = new List<File>();
+        //    //DataTable 
+        //    if (files != null)
+        //    {
+        //        var Extension = Path.GetExtension(files.FileName);
+        //        var fileName = "my-file-" + DateTime.Now.ToString("ddMMyyyyHHmmssfff") + Extension;
+        //        string path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+        //        model.FileUrl = Url.Content(Path.Combine("~/UploadedFiles/", fileName));
+        //        model.FileName = fileName;
+        //    }
+        //        return View();
+        //}
 
         // POST: Tables/Create
         [HttpPost]
